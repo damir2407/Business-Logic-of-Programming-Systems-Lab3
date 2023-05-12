5c0e38228d5cc24d4b463c793e9d7521775040fc
Daniil <twilightnotch@gmail.com>
2023-05-12 23:35:35 +0300

    Update pom.xml

--- main_service/src/main/java/com/example/main_service/MainServiceApplication.java
+++ main_service/src/main/java/com/example/main_service/MainServiceApplication.java
@@ -3,7 +3,7 @@ package com.example.main_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.data", "com.example.main_service"})
 public class MainServiceApplication {
 
     public static void main(String[] args) {
