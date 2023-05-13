41c0e3424ef7e96702609d832b9c731707f91c26
Daniil <twilightnotch@gmail.com>
2023-05-13 18:27:48 +0300

    Add transaction manager

---
+++ main_service/src/main/java/com/example/main_service/security/CookUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.main_service.security;
+
+import com.example.data.model.basic.User;
+import com.example.data.repository.basic.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
+public class CookUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CookUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDetails loadUserByUsernameAndRoles(String login, Collection<? extends GrantedAuthority> roles) throws UsernameNotFoundException {
+        return CookUserDetails.build(login, roles);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = userRepository.
+                findByLogin(login)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователя с логином " + login + " не существует"));
+        return CookUserDetails.build(user);
+    }
+}
